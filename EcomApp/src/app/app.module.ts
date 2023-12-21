6b951ee56150adc1cd3d4a36d52257b59ef32a69
Kivuila Nahel <nahel.kivuila@gmail.com>
2023-12-21 12:26:12 +0100

    feat: add welcome page and shopping cart

--- EcomApp/src/app/app.module.ts
+++ EcomApp/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './pages/accueil/accueil.component';
+import { ShoppingCartComponent } from './pages/shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule,
