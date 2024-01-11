dd1ba3134c9bb0f1595e7bcee9325ef295a7ee1c
Kivuila Nahel <nahel.kivuila@gmail.com>
2024-01-11 12:53:52 +0100

    feat: add edit page

--- EcomApp/src/app/app-routing.module.ts
+++ EcomApp/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {AccueilComponent} from "./pages/accueil/accueil.component";
 import {ShoppingCartComponent} from "./pages/shopping-cart/shopping-cart.component";
+import {AddProductComponent} from "./pages/add-product/add-product.component";
 
 const routes: Routes = [
   {
@@ -13,6 +14,11 @@ const routes: Routes = [
     path: 'cart',
     title: 'Shopping Cart',
     component: ShoppingCartComponent
+  },
+  {
+    path: 'edit',
+    title: 'Edit Page',
+    component: AddProductComponent
   }
 ];
 
--- EcomApp/src/app/app.module.ts
+++ EcomApp/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AccueilComponent } from './pages/accueil/accueil.component';
 import { ShoppingCartComponent } from './pages/shopping-cart/shopping-cart.component';
+import { AddProductComponent } from './pages/add-product/add-product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AccueilComponent,
-    ShoppingCartComponent
+    ShoppingCartComponent,
+    AddProductComponent
   ],
   imports: [
     BrowserModule,
