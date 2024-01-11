612406f0e69b07552f274ad320ce03d39a97131b
Kivuila Nahel <nahel.kivuila@gmail.com>
2024-01-11 12:27:55 +0100

    feat: add shopping cart html

--- EcomApp/src/app/app-routing.module.ts
+++ EcomApp/src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {AccueilComponent} from "./pages/accueil/accueil.component";
+import {ShoppingCartComponent} from "./pages/shopping-cart/shopping-cart.component";
 
 const routes: Routes = [
   {
     path: '',
     title: 'Accueil',
     component: AccueilComponent
+  },
+  {
+    path: 'cart',
+    title: 'Shopping Cart',
+    component: ShoppingCartComponent
   }
 ];
 
