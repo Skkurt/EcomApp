c67e215c0a6a9a4381f022f8ca979e4aa4083b1b
jgachet-1046 <jeremy.jgachet@gmail.com>
2024-02-08 13:04:44 +0100

    [WIP] Commit #3

--- EcomApp/src/app/app.module.ts
+++ EcomApp/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AccueilComponent } from './pages/accueil/accueil.component';
 import { ShoppingCartComponent } from './pages/shopping-cart/shopping-cart.component';
 import { AddProductComponent } from './pages/add-product/add-product.component';
-import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
