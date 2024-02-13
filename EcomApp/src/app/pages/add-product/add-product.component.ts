318388e162f76df807cbe4e581f441415f15ff05
jgachet-1046 <jeremy.jgachet@gmail.com>
2024-02-13 14:36:16 +0100

    [WIP] Refactor the code and fix the product update function to work

--- EcomApp/src/app/pages/add-product/add-product.component.ts
+++ EcomApp/src/app/pages/add-product/add-product.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Prime } from 'src/app/models/prime';
 import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-addproduct',
@@ -10,13 +11,10 @@ import { Observable } from 'rxjs';
 })
 export class AddProductComponent {
   products!: Observable<Prime[]>;
-  product: any;
+  product!: Observable<Prime>;
   isPopupOpen = false;
-  input: HTMLInputElement | null;
 
-  constructor(private http: HttpClient) {
-    this.input = document.getElementById('price') as HTMLInputElement | null;
-  }
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.fetchData();
@@ -33,38 +31,37 @@ export class AddProductComponent {
 
   fetchData() {
     const url = 'http://localhost:3000/items';
-
-    this.products = this.http.get<Prime[]>(url)
+    this.products = this.http.get<Prime[]>(url);
   }
 
   getItemById(id: number) {
     const url = 'http://localhost:3000/items/' + id;
+    this.product = this.http.get<Prime>(url);
+  }
 
-    this.http.get(url).subscribe(
-      (response) => {
-        this.product = response;
-        console.log('Données récupérées avec succès :', this.product);
-      },
-      (error) => {
-        console.error('Erreur lors de la récupération des données :', error);
+  editProduct(id: number, newPrice: string) {
+    if (newPrice === undefined) {
+      console.error('Nouveau prix non défini');
+      return;
+    }
+    
+    const url = 'http://localhost:3000/items/' + id;
+    this.http.get<Prime>(url).pipe(
+      switchMap((product) => {    
+        product.price = parseInt(newPrice);   
+        return this.http.put<Prime>(url, product);    
+      })  
+    ).subscribe(
+      {
+        next: (updatedProduct) => {
+          console.log('Produit modifié avec succès :', updatedProduct);
+          this.fetchData();
+          this.closePopup();
+        },
+        error: (error) => {
+          console.error('Erreur lors de la modification du produit :', error);
+        }
       }
     );
   }
-
-  editProduct(id: number) {
-    const url = 'http://localhost:3000/items/' + id;
-
-    var newPrice = this.input?.value;
-
-    this.http.post(url, { price: newPrice }).subscribe(
-      (response) => {
-        this.fetchData();
-        console.log('Produit modifié avec succès :', response);
-      },
-      (error) => {
-        console.error('Erreur lors de la modification du produit :', error);
-      }
-    );
-}
-
 }
