699546120f8d44389fadf00e47197f2303fec39c
Kivuila Nahel <nahel.kivuila@gmail.com>
2024-03-07 16:38:06 +0100

    feat: add basket logic

--- EcomApp/src/app/pages/accueil/accueil.component.ts
+++ EcomApp/src/app/pages/accueil/accueil.component.ts
@@ -1,14 +1,16 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Prime} from "../../models/prime";
-import {Cart} from "../../models/cart";
 import {HttpClient} from "@angular/common/http";
+import {tap} from "rxjs";
 
 @Component({
   selector: 'app-accueil',
   templateUrl: './accueil.component.html',
   styleUrls: ['./accueil.component.css']
 })
-export class AccueilComponent {
+export class AccueilComponent implements OnInit {
+
+  primes!: Prime[];
 
 
   constructor(
@@ -16,52 +18,25 @@ export class AccueilComponent {
   ) {
   }
 
-  primes = this.httpClient.get<Prime[]>('http://localhost:3000/items')
-  primes1: Prime[] = [
-    {
-      id: 1,
-      title: "Prime 1",
-      description: 'Description 1',
-      price: 4,
-      image: "https://www.unitedsweets.co.nz/cdn/shop/files/2a_1024x1024.png?v=1693549862"
-    },
-    {
-      id: 2,
-      title: "Prime 2",
-      description: 'Description 2',
-      price: 4,
-      image: "https://www.unitedsweets.co.nz/cdn/shop/files/PrimeStrawberry_1024x1024.png?v=1700984118"
-    },
-    {
-      id: 3,
-      title: "Prime 3",
-      description: 'Description 3',
-      price: 6,
-      image: "https://www.sportsdirect.com/images/imgzoom/71/71065903_xxl.jpg"
-    },
-    {
-      id: 4,
-      title: "Prime 4",
-      description: 'Description 4',
-      price: 4,
-      image: "https://i.ebayimg.com/images/g/CtIAAOSwXoBkZLUo/s-l1600.jpg"
-    }
-  ]
-
-  cartList: Cart[] = []
+  ngOnInit() {
+    this.httpClient.get<Prime[]>('http://localhost:3000/items').pipe(
+      tap(primes => {
+        this.primes = primes
+      })
+    ).subscribe()
+  }
 
 
   addToCart(prime: Prime) {
-    const newCart: Cart = {id: parseInt((Math.random()*10000).toString()), items: prime}
 
-    let items = localStorage.getItem('items')
+    const items = localStorage.getItem('items')
+    let cart = [];
 
-    if (items) {
-      this.cartList = JSON.parse(items)
-    }
+    if (items)
+      cart = JSON.parse(items);
 
-    this.cartList.push(newCart)
+    cart.push(prime)
+    localStorage.setItem('items', JSON.stringify(cart));
 
-    localStorage.setItem('items', JSON.stringify(this.cartList))
   }
 }
