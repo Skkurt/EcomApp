31f793690bbb8357cf224fcdab365053ff455a82
PacoGalasso <pacogalasso@gmail.com>
2024-04-19 15:08:16 +0200

    shopping cart

--- EcomApp/src/app/pages/shopping-cart/shopping-cart.component.ts
+++ EcomApp/src/app/pages/shopping-cart/shopping-cart.component.ts
@@ -1,37 +1,34 @@
-import {Component, OnInit} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Prime} from "../../models/prime";
-import {tap} from "rxjs";
-import {ShoppingCart} from "../../models/cart";
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Prime } from '../../models/prime';
+import { Observable, tap } from 'rxjs';
+import { ShoppingCart } from '../../models/cart';
 
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
   styleUrls: ['./shopping-cart.component.css'],
 })
-
-export class ShoppingCartComponent implements OnInit{
-
+export class ShoppingCartComponent implements OnInit {
   primes!: Prime[];
   reduceList!: Prime[];
 
-  testArray!: ShoppingCart[]
+  testArray!: ShoppingCart[];
 
+  totalPrice!: number;
+  toDelete!: boolean;
 
-
-  constructor(
-    private readonly httpClient: HttpClient
-  ) {
-  }
+  constructor(private readonly http: HttpClient) {}
 
   ngOnInit() {
-    const items = localStorage.getItem('items')
-    if (items)
-      this.reduceList = JSON.parse(items)
+    const items = localStorage.getItem('items');
+    if (items) this.reduceList = JSON.parse(items);
 
-    const test = this.reduceList
+    const test = this.reduceList;
+    console.log(items);
+    this.primes = this.reduceList;
+    this.totalPrice = this.getAllPrices(this.reduceList);
 
-    console.log(test)
     // this.primes = this.reduceList
     // this.httpClient.get<Prime[]>('http://localhost:3000/items').pipe(
     //   tap(primes => {
@@ -40,4 +37,23 @@ export class ShoppingCartComponent implements OnInit{
     // ).subscribe()
   }
 
+  getAllPrices(primeList: Prime[]): number {
+    const total: number = primeList.reduce(
+      (prix, prime) => (prix += prime.price),
+      0
+    );
+    console.log(total);
+    return total;
+  }
+  deleteProduct(id: number) {
+    const url = 'http://localhost:3000/items/' + id;
+    this.http.delete<Prime>(url).subscribe({
+      next: (deletedProduct) => {
+        console.log('Produit supprimé avec succès :', deletedProduct);
+      },
+      error: (error) => {
+        console.error('Erreur lors de la suppression du produit :', error);
+      },
+    });
+  }
 }
